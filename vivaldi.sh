b472d3bb9f50bcf382fd5decf1e26cd3c66734fa
Dzheremi-belpois <chichvarov1315@gmail.com>
2023-06-27 19:59:08 +0300

    Changed package name in Vivaldi.sh

--- vivaldi.sh
+++ vivaldi.sh
@@ -4,6 +4,6 @@ sudo chown $USER vivaldi.repo
 sudo cp vivaldi.repo /etc/yum.repos.d/
 sudo dnf update
 
-sudo dnf install vivaldi
+sudo dnf install vivaldi-stable
 
 zenity --info --text "Everything done seccesfully, maybe..."
